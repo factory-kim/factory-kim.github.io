8bb78c552c4a1d11502b08088b3cb1349f61db88
Jisoo <sjw03151@naver.com>
2023-08-30 00:15:56 +0900

    Change test_name to test_id

---
+++ data/1/1_meta.js
@@ -0,0 +1,11 @@
+export const test_info = {
+    testID: 1, // test unique ID
+    language: ["kor", "eng", "chn", "jpn"], // language support
+    total_number_of_questions: 8
+}
+
+export const algorithm = { // 모든 경우 커버해야함
+    // TODO
+    /*1번 ans == 8 → skin type,
+    1번 ans == 7 → sexy type,*/
+}
