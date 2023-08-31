2f2269152539826a5f0c39338e075b03a6170555
JJ <jaewon.jo@geappliances.com>
2023-08-31 17:02:57 +0900

    minor changes

--- js/start.js
+++ js/start.js
@@ -6,26 +6,26 @@ const start_image   = document.querySelector(".responsive-image")
 const start_button  = document.querySelector(".start-button")
 const description   = document.querySelector(".description")
 
-function setupInformation(test_info, test_id, lan) {
+function setupInformation(test_info, test_id, language) {
     title.innerText = test_info.title
     subtitle.innerText = test_info.subtitle
     description.innerText = test_info.description
-    start_image.src = getStartImagePath(test_id, lan)
+    start_image.src = getStartImagePath(test_id, language)
 }
 
 function addEventStartButton() {
     start_button.addEventListener("click", function(e){
-        console.log("Go to next page")    
+        console.log("Go to next page")
     });
 }
 
 function init() {
     const test_id = 1 // FIX ME
-    const lan = 'kor'
+    const language = 'kor'
 
-    const test_info = getCoverPageInfo(test_id, lan)
+    const test_info = getCoverPageInfo(test_id, language)
 
-    setupInformation(test_info, test_id, lan)
+    setupInformation(test_info, test_id, language)
     addEventStartButton()
 }
 
