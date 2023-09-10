8effdfe7a6a922933d2c9e99774dc0184c9aedf4
Jisoo <sjw03151@naver.com>
2023-09-10 16:43:27 +0900

    Apply real data on getting type

--- data/1/1_meta.js
+++ data/1/1_meta.js
@@ -1,11 +1,17 @@
 export const test_info = {
     test_id: 1, // unique ID
     supported_languages: ["kor", "eng", "chn", "jpn"],
-    total_number_of_questions: 8
+    total_number_of_questions: 11
 }
 
-export const algorithm = { // 모든 경우 커버해야함
-    // TODO
-    /*1번 ans == 8 → skin type,
-    1번 ans == 7 → sexy type,*/
+export const algorithm = {
+    num: [10,8,6,4,2,0],
+    types: [
+        "emotional_connector",
+        "attachment_and_intimacy_seeker",
+        "stable_pleasure_seeker",
+        "curiosity_and_novelty_pursuer",
+        "pleasure_and_joy_seeker",
+        "balance_and_harmony_seeker"
+    ]
 }
